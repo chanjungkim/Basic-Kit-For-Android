5488fcf05fdcd077ba25cc47670f730fe786b836
Chanjung Kim <fman1335@gmail.com>
2019-04-18 00:40:50 +0900

    Update UILib.java

--- basickit/lib/UILib.java
+++ basickit/lib/UILib.java
@@ -1,11 +1,11 @@
-package org.koreanlab.message_simulator.lib;
+package org.koreanlab.lib;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import androidx.annotation.NonNull;
 
 public class UILib {
     private static Context mContext;
@@ -24,35 +24,44 @@ public class UILib {
         return instance;
     }
 
-    public void setHideNavigation(boolean flag) {
+    public UILib setHideNavigation(boolean flag) {
         // 아이스크림 샌드위치(4.0) 이상일 경우
         if (Build.VERSION.SDK_INT >= 14 && flag) {
             newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         }
+
+        return instance;
     }
 
-    public void setFullScreen(boolean flag){
+    public UILib setFullScreen(boolean flag) {
         // 젤리빈(4.1) 이상일 경우
         if (Build.VERSION.SDK_INT >= 16 && flag) {
             newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
         }
+
+        return instance;
     }
 
-    public void setImmerseiveSticky(){
+    public UILib setImmerseiveSticky() {
         // 킷캣(4.4) 이상일 경우
         if (Build.VERSION.SDK_INT >= 18) {
             newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            return instance;
         }
+
+        return instance;
     }
 
-    public void setStatusBarColor(int color) {
+    public UILib setStatusBarColor(int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // finally change the color
-            ((Activity)mContext).getWindow().setStatusBarColor(color);
+            ((Activity) mContext).getWindow().setStatusBarColor(color);
         }
+
+        return instance;
     }
 
-    public void apply(){
+    public void apply() {
         ((Activity) mContext).getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
     }
 }
